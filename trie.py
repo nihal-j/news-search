63a8422fd5b8ef3bc9af7874b1c34ed0b75a6c12
Nihal Jain <jain.nihal98@gmail.com>
2019-10-15 01:24:03 +0530

    normalization done to remove accents

--- trie.py
+++ trie.py
@@ -9,14 +9,11 @@ class Node:
         
     def add(self, suffix, currentIndex):
         
-        print (suffix)
-        
         if currentIndex == len(suffix):
             self.words = self.words + 1
             self.prefixes = self.prefixes + 1
         
         else:
-            print(ord(suffix[currentIndex]))
             self.prefixes = self.prefixes + 1
             firstChar = suffix[currentIndex]
             if self.children[ord(firstChar)] == None:
@@ -60,15 +57,12 @@ class CollectionNode(Node):
         
     def add_(self, suffix, currentIndex, docID):
         
-#         print (suffix)
-        
         if currentIndex == len(suffix):
             self.words = self.words + 1
             self.prefixes = self.prefixes + 1
             self.documents.add(docID)
         
         else:
-#             print(ord(suffix[currentIndex]))
             self.prefixes = self.prefixes + 1
             firstChar = suffix[currentIndex]
             if self.children[ord(firstChar)] == None:
@@ -88,20 +82,4 @@ class CollectionNode(Node):
                 return []
             else:
                 currentIndex += 1
-                return self.children[ord(firstChar)].get_doc_list(suffix, currentIndex) 
-
-## Test code
-
-# root = Node()
-# root.add('hello', 0)
-# root.add('hell', 0)
-# root.add('adc', 0)
-# root.add('heafth', 0)
-# print(
-# root.count_prefixes('hel', 0),
-# root.count_prefixes('hea', 0),
-# root.count_words('hello',0),
-# root.count_words('hell', 0),
-# root.count_words('adc', 0),
-# # root.count_words('hellodd',0))
-print('dd')
+                return self.children[ord(firstChar)].get_doc_list(suffix, currentIndex)
